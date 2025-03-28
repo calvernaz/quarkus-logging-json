5746c0d51912aec02fde78843534f9574bbb63e4
Cesar Alvernaz <cesar.alvernaz@workday.com>
2025-03-28 08:53:50 +0000

    migrate to ConfigMapping configs

--- runtime/src/main/java/io/quarkiverse/loggingjson/config/ConfigFormatter.java
+++ runtime/src/main/java/io/quarkiverse/loggingjson/config/ConfigFormatter.java
@@ -2,10 +2,10 @@ package io.quarkiverse.loggingjson.config;
 
 public interface ConfigFormatter {
 
-  /**
-   * Determine whether the formatter is enabled.
-   *
-   * @return true if the formatter is enabled, false otherwise
-   */
-  boolean isEnabled();
+    /**
+     * Determine whether the formatter is enabled.
+     *
+     * @return true if the formatter is enabled, false otherwise
+     */
+    boolean isEnabled();
 }
